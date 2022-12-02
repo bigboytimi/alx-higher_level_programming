18e8a2e48feeeac98de17b7a068b74d14444c633
bigboytimi <olutimiolowookere@icloud.com>
2022-12-02 19:34:02 +0000

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
     arg_num = len(sys.argv) - 1
 
     if arg_num == 0:
-        print("{} arguments".format(arg_num))
+        print("{} arguments.".format(arg_num))
     elif arg_num == 1:
-        print("{} argument:")
+        print("{} argument:".format(arg_num))
         print("{}: {}".format(arg_num, arg_num, sys.argv[1]))
     else:
 
