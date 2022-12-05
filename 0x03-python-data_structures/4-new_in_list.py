c0e7c4d67a27335b56e1fef3bd538df26df20fe2
bigboytimi <olutimiolowookere@icloud.com>
2022-12-05 22:29:29 +0000

    Task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    fresh_list = my_list.copy()
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    else:
+        fresh_list[idx] = element
+        return fresh_list
+
+
