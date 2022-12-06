ea9918210509285e0d3e6bff0980155a33afe251
bigboytimi <olutimiolowookere@icloud.com>
2022-12-06 13:50:12 +0000

    Task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,10 @@
 
 def max_integer(my_list=[]):
     max_value = 0
-    for i in my_list:
-        if i > max_value:
-            max_value = i
+    if my_list == []:
+        return 'None'
+    else:
+        for i in my_list:
+            if i > max_value:
+                max_value = i
     return max_value
