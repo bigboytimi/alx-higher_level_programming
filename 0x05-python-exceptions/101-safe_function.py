5212d403c6cc48143d2a0f9e6f223a657d747e47
bigboytimi <olutimiolowookere@icloud.com>
2022-12-20 21:51:02 +0000

    Task 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,9 +2,10 @@
 
 
 def safe_function(fct, *args):
+    import sys
     try:
         result = fct(*args)
         return result
     except Exception as exerr:
-        print("Exception: {}".format(exerr))
+        print("Exception: {}".format(exerr), file=sys.stderr)
         return None
