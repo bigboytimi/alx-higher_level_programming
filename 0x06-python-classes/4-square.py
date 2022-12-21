6238e52cd8f4b5444451f0dd9ff7e607ff0b7380
bigboytimi <olutimiolowookere@icloud.com>
2022-12-21 15:02:21 +0000

    Task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Using getters and setters within a class."""
+
+
+class Square:
+    """A class called Square."""
+
+    def __init__(self, size=0):
+        """Method to assign the size of a square.
+
+        Args:
+            size(int) = size of a square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value: value to assign to the square.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        result = self.__size**2
+        return result
