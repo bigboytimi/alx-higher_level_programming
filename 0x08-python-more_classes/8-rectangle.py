2324c47033124bc6b09bb3420a7e84aa31f9c27f
bigboytimi <olutimiolowookere@icloud.com>
2023-01-05 14:27:46 +0000

    compare rectangles

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -56,6 +56,18 @@ class Rectangle:
             result = 2 * (self.__width + self.__height)
             return result
 
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+
+        if rect_1.area() >= rect_2.area():
+            return rect_1.area()
+        else:
+            return rect_2.area()
+
     def __str__(self):
         """print the rectangle with the character ``#``"""
 
@@ -77,22 +89,3 @@ class Rectangle:
         type(self).number_of_instances -= 1
         print("Bye rectangle...")
 
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """Return the Rectangle with the bigger area.
-
-        Args:
-            rect_1 (Rectangle) : First rectangle
-            rect_2 (Rectangle) : Second rectangle
-        Raises:
-            TypeError: If either of both is not a rectangle
-        """
-
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise ValueError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() > rect_2.area():
-            return rect_1
-        else:
-            return rect_2
