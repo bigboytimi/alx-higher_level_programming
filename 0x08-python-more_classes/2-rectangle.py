3cf284b73df585288a48df39ebe79be6ea007d92
bigboytimi <olutimiolowookere@icloud.com>
2023-01-05 01:53:46 +0000

    Area and perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -3,44 +3,51 @@
 
 
 class Rectangle:
-    """Define a rectangle"""
+    """Initialize a class Rectangle."""
 
     def __init__(self, width=0, height=0):
-        """Instantiate a new Rectangle Object
+        """Initialize a new Rectangle object
 
         Args:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
-
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
-        """Get/set the width of the rectangle."""
+        """Get/set the width of the Rectangle."""
         return self.__width
-            
 
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must be an >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
-
 
     @property
     def height(self):
-        """Get/set the height of the rectangle"""
         return self.__height
-        
 
     @height.setter
     def height(self, value):
+        """Get/set the height of the rectangle."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        result = self.__width * self.__height
+        return result
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            result = 2 * (self.__width + self.__height)
+            return result
