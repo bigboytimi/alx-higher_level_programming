24e4891400881f81469d7c9e27b1b98c26ebebeb
bigboytimi <olutimiolowookere@icloud.com>
2023-01-09 15:56:06 +0000

    same or inherit

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Check if obj is an instance of class or subclass"""
 
+
 def is_kind_of_class(obj, a_class):
     """Return True if it is an instance, return false if not"""
     if isinstance(obj, a_class):
