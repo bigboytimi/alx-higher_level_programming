6f3cefdfef2a67a78aec5fb0e4226c773825502a
bigboytimi <olutimiolowookere@icloud.com>
2023-01-09 15:45:26 +0000

    confirm instance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Confirm instance."""
+
+def is_same_class(obj, a_class):
+    """Return True if object is instance of a specified class"""
+    if type(obj) == a_class:
+        return True
+    return False
