e6a0afc7de0fa68fe1f1351f9d283020157ffebd
bigboytimi <olutimiolowookere@icloud.com>
2023-01-10 19:26:31 +0000

    2app

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,6 +6,3 @@ def append_write(filename="", text=""):
     """Returns number of characters added."""
     with open(filename, "a", encoding="utf-8") as new_file:
         new_file.write(text)
-
-        number = new_file.tell()
-        return number
