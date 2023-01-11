3e15af8e7904cab9f65cb2fcdd2eff0a379652ee
bigboytimi <olutimiolowookere@icloud.com>
2023-01-11 13:59:17 +0000

    100app

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-
+"""Define a line of text to a file."""
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """Appends a text after a particular text in a row."""
     text = ""
     with open(filename, "r") as nf:
         for line in nf:
