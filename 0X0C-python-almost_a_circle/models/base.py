b66066da68505a57aad031df17603853bd3f0956
bigboytimi <olutimiolowookere@icloud.com>
2023-01-12 18:45:12 +0000

    bpy

---
+++ 0X0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Create a class Base."""
+
+
+class Base:
+    """A class called Base."""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """The definition for class Base."""
+        if id is not None:
+            self.id = id
+        Base.__nb_objects += 1
+        self.id = Base.__nb_objects
