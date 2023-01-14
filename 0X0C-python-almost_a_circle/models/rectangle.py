5548c4a48b274601c71e103a73636d6359fb388e
bigboytimi <olutimiolowookere@icloud.com>
2023-01-14 14:50:33 +0000

    9rect

--- 0X0C-python-almost_a_circle/models/rectangle.py
+++ 0X0C-python-almost_a_circle/models/rectangle.py
@@ -64,7 +64,8 @@ class Rectangle(Base):
         """return the area of rectangle"""
         return self.__width * self.__height
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
+
         if args and len(args) != 0:
             a = 0
             for arg in args:
@@ -82,6 +83,22 @@ class Rectangle(Base):
                 elif a == 4:
                     self.y = arg
                 a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
 
     def display(self):
         """prints to stdout."""
