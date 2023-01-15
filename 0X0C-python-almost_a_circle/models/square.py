c23103ffcab142dd75d72a08275ba7c33a818514
bigboytimi <olutimiolowookere@icloud.com>
2023-01-15 08:21:50 +0000

    sqaure

--- 0X0C-python-almost_a_circle/models/square.py
+++ 0X0C-python-almost_a_circle/models/square.py
@@ -20,3 +20,12 @@ class Square(Rectangle):
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
                                                  self.y, self.width)
+
+    def to_dictionary(self):
+        """Return the dictionary representation of Square."""
+        return {
+                "id": self.id,
+                "size": self.width,
+                "x": self.x,
+                "y": self.y
+                }
