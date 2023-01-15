e5f7555f376401352668b734fc1db1eee5fcd2d9
bigboytimi <olutimiolowookere@icloud.com>
2023-01-15 12:49:45 +0000

    jsontofile

--- 0X0C-python-almost_a_circle/models/base.py
+++ 0X0C-python-almost_a_circle/models/base.py
@@ -16,9 +16,31 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
-        if list_dictionaries is None:
+        """return json string representation
+
+
+        Args: 
+            list_dictionaries(list): A list of dictionaries
+        """
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
-        else:
-            result = json.dumps(list_dictionaries)
-            return result
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the json representation to file
+
+
+        Args:
+            list_objs(list): A list of inherited Base instances
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as e:
+            if list_objs is None:
+                e.write([])
+            else:
+                list_dicts = [inst.to_dictionary() for inst in list_objs]
+                e.write(Base.to_json_string(list_dicts))
+
