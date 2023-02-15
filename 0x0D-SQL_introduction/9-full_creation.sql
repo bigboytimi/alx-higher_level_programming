eb595fc77f35e1eff6acef01d562d068d63b2ff0
bigboytimi <olutimiolowookere@icloud.com>
2023-02-15 12:26:02 +0000

    create a table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,5 @@
 -- create a table and assign rows
+
 CREATE TABLE IF NOT EXISTS `second_table`(`id` INT, `name` VARCHAR(256), `score` INT)
 INSERT INTO `second_table` (`id`, `name`) VALUES (1, "John", 10)
 INSERT INTO `second_table` (`id`, `name`) VALUES(2, "Alex", 3)
