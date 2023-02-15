2a32555a20a215dc3dd7120438127824a041497e
bigboytimi <olutimiolowookere@icloud.com>
2023-02-15 12:14:40 +0000

    move-to-utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,5 @@
 -- convert database to utf8
+
 USE `hbtn_0c_0`
-ALTER `first_table`
+ALTER TABLE `first_table`
 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,5 @@
 -- list records of the table
+
 SELECT `score`, `name`
 FROM `second_table`
 WHERE `name` != ""
