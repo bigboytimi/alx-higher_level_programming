9fcdc97e837c343bde983e0aed94d604e965eae4
bigboytimi <olutimiolowookere@icloud.com>
2023-02-16 20:21:37 +0000

    0-privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- List the privileges of users
 
-SHOW GRANTS FOR 'user-0d_1'@'localhost';
-SHOW GRANTS FOR 'user-0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
